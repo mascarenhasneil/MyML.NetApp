81b5e888b906de1f8db9da6cb6fcd96a27c19c28
Neil Mascarenhas <neilpm1995@gmail.com>
2020-10-08 16:25:06 +0530

    Add project files.

---
+++ MyML.NetApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyML.NetApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
